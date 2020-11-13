d6895c759b0af05731700ea895c6a43cf109de2d
Marcus Mesta <marcusmesta@protonmail.com>
2020-11-12 22:55:49 -0800

    Refactoring server index browser to use file structure

--- src/dmsg_gui/window_functions.go
+++ src/dmsg_gui/window_functions.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"sort"
 )
 
 //ServerPageCountMax holds the max page give the current server's index
@@ -53,6 +54,111 @@ func renderServerBrowser() {
 	fmt.Println(divider)
 	fmt.Println(pageStatus)
 	fmt.Println("<< B  |  N >>")
+}
+
+func RenderServerBrowser2() {
+	currendDirPtr := &rootDir
+	bufferHeight := 7
+	terminalHeight, err := sttyWrapperGetTerminalHeight()
+	if err != nil {
+		terminalHeight = 10 //default on error
+
+	} else {
+		terminalHeight = terminalHeight - bufferHeight
+
+	}
+	terminalWidth, err := sttyWrapperGetTerminalWidth()
+	if err != nil {
+		fmt.Println(err)
+	}
+	ServerPageCountMax = len(CurrentServerIndex) / terminalHeight
+	pageRemainder := len(CurrentServerIndex) % terminalHeight
+	if pageRemainder > 0 {
+		ServerPageCountMax++
+	}
+	// Avoid 1/0 pages
+	if ServerPageCountMax == 0 {
+		ServerPageCountMax = 1
+	}
+	pageStatus := fmt.Sprintf("page (%d / %d)", DownloadBrowserIndex, ServerPageCountMax)
+
+	divider := ""
+	for i := 0; i < terminalWidth; i++ {
+		divider += "="
+	}
+	ClearScreen()
+	fmt.Println(divider)
+	fmt.Println("SERVER DOWNLOAD INDEX")
+	fmt.Println(divider)
+	currendDirPtr = currendDirPtr.subDirs["Cowboy_Bebop"]
+	renderDirectories(currendDirPtr, terminalWidth)
+	renderFiles(currendDirPtr, terminalWidth)
+	fmt.Println(divider)
+	fmt.Println(pageStatus)
+	fmt.Println("<< B  |  N >>")
+}
+
+func renderDirectories(dirPtr *Directory, terminalWidth int) {
+	//sort sub dirs A-Z
+	var subDirKeys []string
+	for key := range dirPtr.subDirs {
+		subDirKeys = append(subDirKeys, key)
+	}
+	sort.Strings(subDirKeys)
+
+	//Render directories
+	renderIndex := 1
+	if len(subDirKeys) > 0 {
+		for _, key := range subDirKeys {
+			itemIndex := renderIndex + (terminalWidth*DownloadBrowserIndex - 1) - terminalWidth + 1
+			if itemIndex-1 < len(dirPtr.subDirs) {
+				fileBuffer := ""
+				tmpEntry := fmt.Sprintf("%d) %s%s", itemIndex, key, "Directory")
+				bufferToAdd := terminalWidth - len(tmpEntry)
+				for i := 0; i < bufferToAdd; i++ {
+					fileBuffer += "-"
+				}
+				listEntry := fmt.Sprintf("%d) %s%s%s", itemIndex, key, fileBuffer, "Directory")
+				fmt.Println(listEntry)
+			} else {
+				fmt.Println("-")
+			}
+			renderIndex++
+		}
+	} else {
+		//fmt.Println("[Empty server index/Could not fetch list]")
+	}
+}
+func renderFiles(dirPtr *Directory, terminalWidth int) {
+	//sort sub dirs A-Z
+	var fileNames []string
+	for key := range dirPtr.files {
+		fileNames = append(fileNames, key)
+	}
+	sort.Strings(fileNames)
+
+	//Render directories
+	itemIndex := 1
+	if len(fileNames) > 0 {
+		for _, key := range fileNames {
+			if true {
+				fileSize := dirPtr.files[key]
+				tmpEntry := fmt.Sprintf("%d) %s%d", itemIndex, key, fileSize)
+				bufferToAdd := terminalWidth - len(tmpEntry)
+				fileBuffer := ""
+				for i := 0; i < bufferToAdd; i++ {
+					fileBuffer += "-"
+				}
+				listEntry := fmt.Sprintf("%d) %s%s%d", itemIndex, key, fileBuffer, fileSize)
+				fmt.Println(listEntry)
+			} else {
+				fmt.Println("-")
+			}
+			itemIndex++
+		}
+	} else {
+		fmt.Println("[Empty Dir]")
+	}
 }
 
 func renderServerIndexBrowser() {
