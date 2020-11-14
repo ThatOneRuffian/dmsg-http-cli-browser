44e17f15a6fc326ed62a75c209fd0444caa74af8
Marcus Mesta <marcusmesta@protonmail.com>
2020-11-13 23:52:56 -0800

    Using new server index function

--- client/client.go
+++ client/client.go
@@ -3,8 +3,8 @@ package main
 import "dmsg_gui"
 
 func main() {
-	dmsg_gui.CreateTest()
-	/*dmsg_gui.ClearScreen()
+
+	dmsg_gui.ClearScreen()
 	dmsg_gui.InitDownloadsFolder()
 	// if config not found then run the first launch wizard
 	if !dmsg_gui.LoadCache() {
@@ -13,6 +13,6 @@ func main() {
 
 	for true {
 		userChoice := dmsg_gui.ServerListMainMenu()
-		dmsg_gui.ServerIndexMenuHandler(userChoice)
-	}*/
+		dmsg_gui.ServerIndexMenuHandler2(userChoice)
+	}
 }
