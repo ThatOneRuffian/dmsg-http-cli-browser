cb661c1bad906d46eb3c4c48c131b4ce08ef1a6f
Marcus Mesta <marcusmesta@protonmail.com>
2020-11-27 19:54:59 -0800

    Refactored entry rendering functions

--- src/dmsggui/window_functions.go
+++ src/dmsggui/window_functions.go
@@ -221,56 +221,10 @@ func renderMetaData(directoryMetaData map[int]map[string]bool, terminalHeightAva
 			for entryName := range directoryMetaData[index] {
 				// if entry is a directory
 				if directoryMetaData[index][entryName] {
-					tmpBasicLineEntry := fmt.Sprintf("%d)  / Directory", index)
-					tmpLineEntry := tmpBasicLineEntry + entryName
-					horizontalFill := ""
-					//detect and format dirs with names that will overflow current terminal width
+					drawDirEntry(entryName, terminalWidthAvailable, index)
 
-					if len(tmpLineEntry) >= terminalWidthAvailable {
-						entryName = truncateStringTo(entryName, len(tmpBasicLineEntry), terminalWidthAvailable)
-					}
-					for i := terminalWidthAvailable - len(tmpLineEntry); i > 0; i-- {
-						horizontalFill += "-"
-					}
-
-					lineEntry := fmt.Sprintf("%d) %s/ %s Directory", index, entryName, horizontalFill)
-
-					fmt.Println(lineEntry)
 				} else {
-					//if entry is a file
-					fileSize := navPtr.files[entryName]
-					fileSizeUnits := ""
-
-					// format file sizes for human readability
-					if fileSize > 1e9 {
-						fileSize /= 1e9
-						fileSizeUnits = "GB"
-					} else if fileSize > 1e6 {
-						fileSize /= 1e6
-						fileSizeUnits = "MB"
-					} else if fileSize > 1e3 {
-						fileSize /= 1e3
-						fileSizeUnits = "KB"
-					} else {
-						fileSizeUnits = "B"
-					}
-
-					//determine fill amount required
-					entryName = strings.ReplaceAll(entryName, "–", "-") //replace em dash with regular dash em dash doesn't render correctly
-					tmpBasicLineEntry := fmt.Sprintf("%d)   %.2f %s", index, fileSize, fileSizeUnits)
-					horizontalFill := ""
-
-					if len(tmpBasicLineEntry)+len(entryName) >= terminalWidthAvailable {
-						entryName = truncateStringTo(entryName, len(tmpBasicLineEntry), terminalWidthAvailable)
-					}
-
-					for i := terminalWidthAvailable - len(entryName+tmpBasicLineEntry); i > 0; i-- {
-						horizontalFill += "-"
-					}
-
-					//draw line
-					lineEntry := fmt.Sprintf("%d) %s %s %.2f %s", index, entryName, horizontalFill, fileSize, fileSizeUnits)
-					fmt.Println(lineEntry)
+					drawFileEntry(entryName, terminalWidthAvailable, index)
 				}
 
 				verticalHeightBuffer--
@@ -287,6 +241,60 @@ func renderMetaData(directoryMetaData map[int]map[string]bool, terminalHeightAva
 		fmt.Println("-")
 	}
 END:
+}
+func drawDirEntry(entryName string, terminalWidthAvailable int, index int) {
+	tmpBasicLineEntry := fmt.Sprintf("%d)  / Directory", index)
+	tmpLineEntry := tmpBasicLineEntry + entryName
+	horizontalFill := ""
+	//detect and format dirs with names that will overflow current terminal width
+
+	if len(tmpLineEntry) >= terminalWidthAvailable {
+		entryName = truncateStringTo(entryName, len(tmpBasicLineEntry), terminalWidthAvailable)
+	}
+	for i := terminalWidthAvailable - len(tmpLineEntry); i > 0; i-- {
+		horizontalFill += "-"
+	}
+
+	lineEntry := fmt.Sprintf("%d) %s/ %s Directory", index, entryName, horizontalFill)
+
+	fmt.Println(lineEntry)
+}
+
+func drawFileEntry(entryName string, terminalWidthAvailable int, index int) {
+	//if entry is a file
+	fileSize := navPtr.files[entryName]
+	fileSizeUnits := ""
+
+	// format file sizes for human readability
+	if fileSize > 1e9 {
+		fileSize /= 1e9
+		fileSizeUnits = "GB"
+	} else if fileSize > 1e6 {
+		fileSize /= 1e6
+		fileSizeUnits = "MB"
+	} else if fileSize > 1e3 {
+		fileSize /= 1e3
+		fileSizeUnits = "KB"
+	} else {
+		fileSizeUnits = "B"
+	}
+
+	//determine fill amount required
+	entryName = strings.ReplaceAll(entryName, "–", "-") //replace em dash with regular dash em dash doesn't render correctly
+	tmpBasicLineEntry := fmt.Sprintf("%d)   %.2f %s", index, fileSize, fileSizeUnits)
+	horizontalFill := ""
+
+	if len(tmpBasicLineEntry)+len(entryName) >= terminalWidthAvailable {
+		entryName = truncateStringTo(entryName, len(tmpBasicLineEntry), terminalWidthAvailable)
+	}
+
+	for i := terminalWidthAvailable - len(entryName+tmpBasicLineEntry); i > 0; i-- {
+		horizontalFill += "-"
+	}
+
+	//draw line
+	lineEntry := fmt.Sprintf("%d) %s %s %.2f %s", index, entryName, horizontalFill, fileSize, fileSizeUnits)
+	fmt.Println(lineEntry)
 }
 
 func truncateStringTo(stringToTruncate string, rawMenuLength int, terminalWidthAvailable int) string {
