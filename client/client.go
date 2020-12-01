0a6ed65b22a444488ef6e7ff9470db1b06c79da6
Marcus Mesta <marcusmesta@protonmail.com>
2020-11-30 19:38:02 -0800

    Formatting

--- client/client.go
+++ client/client.go
@@ -9,28 +9,31 @@ func main() {
 
 	dmsggui.ClearScreen()
 	dmsggui.InitProgramWorkingDir()
-	dmsggui.DownloadLocationUserInput = dmsggui.InitDownloadsFolder("")
 	dmsggui.RetryAttemptsUserInput = "3"
-	//if config not found then run the first launch wizard
+	dmsggui.DownloadLocationUserInput = dmsggui.InitDownloadsFolder("")
 
 	//parse user arguments
 	flag.StringVar(&dmsggui.DownloadLocationUserInput, "d", dmsggui.DownloadLocationUserInput, "Specify directory to download files to.")
 	flag.StringVar(&dmsggui.RetryAttemptsUserInput, "r", dmsggui.RetryAttemptsUserInput, "Specify number of download attempts.")
 
 	flag.Parse()
+
 	//format user provided dir path
 	pathByteArray := []byte(dmsggui.DownloadLocationUserInput)
 	lastByteChar := pathByteArray[len(pathByteArray)-1]
 	const forwardSlash byte = 92
 	const backSlash byte = 47
+
 	if lastByteChar == forwardSlash || lastByteChar == backSlash {
 		pathByteArray := []byte(dmsggui.DownloadLocationUserInput)
 		lastCharDropped := pathByteArray[:len(pathByteArray)-1]
 		dmsggui.MainDownloadsLoc = string(lastCharDropped)
 	}
+
 	dmsggui.InitDownloadsFolder(dmsggui.DownloadLocationUserInput)
 
 	//attempt to load server cache
+	//if config not found then run the first launch wizard
 	if !dmsggui.LoadCache() {
 		dmsggui.FirstRunWizard()
 	}
